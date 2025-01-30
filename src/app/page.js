e6fbbaf64979b0f4a4057148b41c4c80200e67b6
Sergio <sergeh96@gmail.com>
2025-01-30 15:25:55 +0000

    initialized nextJS project, ticket #2

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+// THIS FILE IS EQUIVALENT TO APP.JSX FOR REACT
+// WHERE WE IMPORT COMPONENTS AND RENDER THEM IN THE DOM TREE
+// ENTRY POINT
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Testing</h1>
+    </div>
+  );
+}
