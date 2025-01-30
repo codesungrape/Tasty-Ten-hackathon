e9a072a4a0fbc6f88438d39b775ecc82304773a2
Sergio <sergeh96@gmail.com>
2025-01-30 15:59:04 +0000

    Header implemented, #8 progress, not yet finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 // THIS FILE IS EQUIVALENT TO APP.JSX FOR REACT
 // WHERE WE IMPORT COMPONENTS AND RENDER THEM IN THE DOM TREE
 // ENTRY POINT
+import Header from "./Components/Header/Header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>Testing</h1>
     </div>
   );
