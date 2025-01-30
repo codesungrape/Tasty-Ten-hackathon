ae3dd51fb925b9f5446559c9fbbd4adefc496407
Sergio <sergeh96@gmail.com>
2025-01-30 16:47:05 +0000

    progress on carousel #13 and cards #14. Need backend data now

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@
 // WHERE WE IMPORT COMPONENTS AND RENDER THEM IN THE DOM TREE
 // ENTRY POINT
 import Header from "./Components/Header/Header";
+import ControlledCarousel from "./Components/RecipeCarousel/RecipeCarousel";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <ControlledCarousel />
       <h1>Testing</h1>
     </div>
   );
